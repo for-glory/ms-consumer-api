e6a3da2f978490b78ed8d299f45cd601217a4cc8
for-glory <forglory0905@outlook.com>
2023-11-06 23:36:43 -0300

    fix: change has to get method

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,7 +12,7 @@ class UserController extends Controller
     public function index(Request $request): JsonResponse
     {
         $users = User::search()
-            ->when($request->has('filter'), function ($query) use ($request) {
+            ->when($request->get('filter'), function ($query) use ($request) {
                 $filter = $request->input('filter');
 
                 return $query->where('multi_match', [
